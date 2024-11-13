72c123f02a82eb8b5406ee95db8a6a2bce07978e
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-11-13 14:14:02 -0600

    feat(app): add debug functionality in struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+#[derive(Debug)]
 pub struct Deck {
     cards: Vec<String>
 }
 
 fn main() {
-    let deck: Deck = Deck{cards: vec![]};
+    let deck: Deck = Deck{cards: Vec::new()};
+    println!("Deck: {:?}", deck)
 }
