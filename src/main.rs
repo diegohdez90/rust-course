21fe0636b54a9f0b83c557190380783d237748ab
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-11-13 14:33:59 -0600

    feat(app): add deck via loop using suited and values

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,19 @@ pub struct Deck {
 }
 
 fn main() {
-    let deck: Deck = Deck{cards: Vec::new()};
+    // list of suites - hearts/spades
+    let suites = vec!["Hearts", "Spades", "Diamonds"];
+    //list of values - ace/two/three
+    let values = vec!["Ace", "Two", "Three"];
+
+    let mut cards = vec![];
+
+    for suit in suites {
+        for value in &values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+    let deck: Deck = Deck{cards: cards};
     println!("Deck: {:?}", deck)
 }
